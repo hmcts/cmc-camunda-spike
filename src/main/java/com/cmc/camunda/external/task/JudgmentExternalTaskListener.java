496ebdc827297a008b2d6b1e40ad8481ae83b181
Karthik Kannan <karthik.kannan@hmcts.net>
2019-09-23 23:17:29 +0100

    Revert "Refactoring"
    This reverts commit 74af075bf6863ff09c655cced7ad1be5c36e750f.

--- src/main/java/com/cmc/camunda/external/task/JudgmentExternalTaskListener.java
+++ src/main/java/com/cmc/camunda/external/task/JudgmentExternalTaskListener.java
@@ -3,34 +3,42 @@ package com.cmc.camunda.external.task;
 import org.camunda.bpm.client.ExternalTaskClient;
 import org.camunda.bpm.client.backoff.ExponentialBackoffStrategy;
 import org.camunda.bpm.client.topic.TopicSubscriptionBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-@Component
+import java.util.HashMap;
+import java.util.Map;
+
 public class JudgmentExternalTaskListener {
+    public static void main(String[] args) {
 
-    private String baseUrl = "http://localhost:8999/rest";
-    private String topicName = "CMC_Judgment";
-    private ExternalTaskClient client;
-    private TopicSubscriptionBuilder subscriptionBuilder;
+        String baseUrl = "http://localhost:8999/rest";
+        String topicName = "CMC_Judgment";
+        String judgementRequired = "true";
 
-    @Autowired
-    public JudgmentExternalTaskListener(JudgmentTaskHandler taskHandler) {
-        client = ExternalTaskClient.create()
+        ExternalTaskClient client = ExternalTaskClient.create()
                 .baseUrl(baseUrl)
                 .lockDuration(6000)
                 .backoffStrategy(new ExponentialBackoffStrategy(500L, 2, 30000L))
                 .maxTasks(2)
                 .build();
-        subscriptionBuilder = client.subscribe(topicName)
-                .variables("caseId")
+
+        TopicSubscriptionBuilder subscriptionBuilder = client.subscribe(topicName)
                 .tenantIdIn("cmc");
-        subscriptionBuilder.handler(taskHandler)
-                .tenantIdIn("cmc")
-                .open();
+
+        subscriptionBuilder.handler(((externalTask, externalTaskService) -> {
+
+            try {
+                String caseId = externalTask.getVariable("caseId");
+                System.out.println("The Case ID working on is ....." + caseId);
+
+
+                Map<String, Object> variablesToBeSupplied = new HashMap<>();
+                variablesToBeSupplied.put("paperResponsePresent", judgementRequired);
+                externalTaskService.complete(externalTask, variablesToBeSupplied);
+            }catch (Exception gene){
+                System.out.println("EXCEPTION>>>>>>" + gene.getMessage());
+                gene.printStackTrace();
+            }
+        })).tenantIdIn("cmc").open();
+
     }
-
 }
-
-
-
