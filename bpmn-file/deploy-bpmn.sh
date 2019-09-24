8ba988ce4e3340b33a16b82d17b982503bcebb07
Karim Lawani <k7lawani@gmail.com>
2019-09-24 16:43:47 +0100

    update deployment

--- bpmn-file/deploy-bpmn.sh
+++ bpmn-file/deploy-bpmn.sh
@@ -4,6 +4,7 @@ set -e
 
 #CAMUNDA_URI="http://localhost:8999"
 CAMUNDA_URI="http://camunda-bpm-pr-8.service.core-compute-preview.internal"
+export http_proxy=http://proxyout.reform.hmcts.net:8080
 
 curl -F deployment-name=cmc-deployment-v2 -F deploy-changed-only=true \
      -F deployment-source=automated -F data=@timer-v2.bpmn \
